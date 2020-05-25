2856729ce7d333aa17721b7e7ace5262f2d8a8c5
Mathieu AMBROISE <mat.ambroise@gmail.com>
2020-05-25 12:19:20 +0200

    Modif des return

--- volaille-exo-master/projet-volaille/src/com/cda/model/VolailleComparator.java
+++ volaille-exo-master/projet-volaille/src/com/cda/model/VolailleComparator.java
@@ -9,31 +9,32 @@ public class VolailleComparator implements Comparator<Volaille> {
 
 	@Override
 	public int compare(Volaille pVolailleA, Volaille pVolailleB) {
-		if (pVolailleA == pVolailleB) {
-			return 0;
-		}
-		if (pVolailleA instanceof VolailleAbattable) {
-			if (pVolailleB instanceof VolailleAbattable) {
-				int vResultat = Float.compare(((VolailleAbattable) pVolailleA).getPoids(),
-						((VolailleAbattable) pVolailleB).getPoids());
-				if (vResultat == 0) {
-					vResultat = pVolailleA.id.compareTo(pVolailleB.id);
+		int vFinal = 0;
+		if (pVolailleA != pVolailleB) {
+			if (pVolailleA instanceof VolailleAbattable) {
+				if (pVolailleB instanceof VolailleAbattable) {
+					int vResultat = Float.compare(((VolailleAbattable) pVolailleA).getPoids(),
+							((VolailleAbattable) pVolailleB).getPoids());
+					if (vResultat == 0) {
+						vResultat = pVolailleA.id.compareTo(pVolailleB.id);
+					}
+					vFinal = vResultat;
+				} else {
+					vFinal = 1;
 				}
-				return vResultat;
 			} else {
-				return 1;
-			}
-		} else {
-			if (pVolailleB instanceof VolailleAbattable) {
-				return -1;
-			} else {
-				int vResultat = ((VolailleAGarder) pVolailleA).getDateDAccueil()
-						.compareTo(((VolailleAGarder) pVolailleB).getDateDAccueil());
-				if (vResultat == 0) {
-					vResultat = pVolailleA.id.compareTo(pVolailleB.id);
+				if (pVolailleB instanceof VolailleAbattable) {
+					vFinal = -1;
+				} else {
+					int vResultat = ((VolailleAGarder) pVolailleA).getDateDAccueil()
+							.compareTo(((VolailleAGarder) pVolailleB).getDateDAccueil());
+					if (vResultat == 0) {
+						vResultat = pVolailleA.id.compareTo(pVolailleB.id);
+					}
+					vFinal = vResultat;
 				}
-				return vResultat;
 			}
 		}
+		return vFinal;
 	}
 }
